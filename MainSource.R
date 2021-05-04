3fb022586c076898b5beb994edaf4a7bfd6a27a2
Kirk Montrose <kirkmontrose@Kirks-MacBook-Air.local>
2021-05-03 20:36:50 -0400

    Final Push

--- MainSource.R
+++ MainSource.R
@@ -9,132 +9,103 @@ library(randomForest)
 library(caret)
 
 TrainA <- read.csv("Data-Source/Train.csv")
-TrainC <- TrainA
 TestSet <- read.csv("Data-Source/Test.csv")#Testing set to extract full data 
 
-#Switching information With Data
-TrainA$Sex                <- factor(TrainA$Sex, levels=c(0,1), labels=c("Female","Male"),   ordered=FALSE)
-TrainA$ChestPain          <- factor(TrainA$ChestPain,  levels=c(0,1,2,3), labels=c("Asymptomatic","Atypical Angina","Non-Anginal Pain","Typical Angina"),  ordered=FALSE)
-TrainA$FastingBloodSugar  <- factor(TrainA$FastingBloodSugar,  levels=c(0,1),labels = c("False","True"),      ordered=FALSE)
-TrainA$RestECG            <- factor(TrainA$RestECG, levels=c(0,1,2),labels=c("LV Hypertrophy","Normal","Abnormalities"), ordered=FALSE)
-TrainA$ExIndAng           <- factor(TrainA$ExIndAng, levels=c(0,1),labels=c("No","Yes"),ordered=FALSE)
-TrainA$Slope              <- factor(TrainA$Slope, levels=c(0,1,2),labels=c("Descending","Flat","Ascending"),ordered=FALSE)
-TrainA$ThalRate           <- factor(TrainA$ThalRate, levels=c(1,2,3),labels=c("Fixed Defect","Normal","Reversible Defect"),ordered=FALSE)
-TrainA$MajorVessels       <- factor(TrainA$MajorVessels,levels=c(0,1,2,3))
-TrainA$Target             <- factor(TrainA$Target,levels=c(0,1),labels = c("<50%",">50%"),ordered = FALSE)
-
-#TrainC$Target             <- factor(TrainA$Target,levels=c(0,1),labels = c("<50%",">50%"),ordered = FALSE)
 # Training and Validation Sets:
-set.seed(18)
+#set.seed(18)
 Train <- sample_frac(TrainA,size = 0.9,replace = FALSE) #Use 90% of the data 
 Test  <- setdiff(TrainA,Train) # 
 
+x <- model.matrix(Target ~ ., Train)[, -1]  #We omit the intercept
+y <- Train$Target
+
+xTest<-model.matrix(Target ~ ., Test)[, -1]
+yTest<-Test$Target
+
+xFinalPreds<-model.matrix( ~ ., TestSet)[, -1]
+yFinalPreds<-Test$Target
+
 #Create Binomial Model
 fit1 <- glm(data=Train, Target~., family="binomial");fit_1_AIC <- AIC(fit1);fit_1_BIC <- BIC(fit1)
-ValB <- mutate(.data=Test,Binnomial=predict(fit1, type="response",newdata = Test))
-ValB <- mutate(.data=ValB,Binnomial=case_when(Binnomial>.5~">50%",TRUE~"<50%"))
+biPRed <- predict(fit1, type="response",newdata = Test)
+Test$Binnomial <- biPRed
 
-confusion1 <- table(ValB$Target,ValB$Binnomial,dnn=c("Actual","Prediction_Binomal_Fulldata"))
-fit_1_Pred <- print(paste("Fraction of Fit1 Correct Predictions:", round((confusion1["<50%","<50%"]+confusion1[">50%",">50%"])/sum(confusion1),4)))
-
-###Create a Lasso Model 
-
-TrainD <- sample_frac(TrainC,size = 0.9,replace = FALSE) #Use 90% of the data 
-TestD  <- setdiff(TrainC,TrainD) # 
-
-x <- model.matrix(Target ~ ., TrainD)[, -1]  #We omit the intercept
-y <- TrainD$Target
-
-
-### Create Tree Model
-
-#Regression Tree
+#Tree
 fitControl <- trainControl(method="cv", number=10)
-model.tree <- train(Target ~ ., data = TrainD,method = "rpart",  trControl = fitControl)
-tree.pred <- predict(model.tree, newdata = TestD)
-ValB$TreeTest <- tree.pred 
-ValB <- mutate(.data=ValB,TreeTest=case_when(TreeTest>.5~">50%",TRUE~"<50%"))
+model.tree <- train(Target ~ ., data = Train,method = "rpart", trControl = fitControl)
+tree.pred <- predict(model.tree, newdata = Test)
+Test$TreeTest <- tree.pred 
 
-confusion2 <- table(ValB$Target,ValB$TreeTest,dnn=c("Actual","Predicted"))
-fit_2_Pred <- print(paste("Fraction of Fit2 Correct Predictions:", round((confusion2["<50%","<50%"]+confusion2[">50%",">50%"])/sum(confusion2),4)))
-
-(MSEtree<-mean((tree.pred-TestD$Target)^2))
-
-#Each node terminal node should have 5 obs at least
-#Each parent node should have at least 10
-
-# Do cross validation to get the best lambda. This does k=10 CV
+#lasso
 cv.out <- cv.glmnet(x,y,alpha=1)
 plot(cv.out)
-(bestlam <- cv.out$lambda.min)
-
-# Compute MSE for the linear regression.
-fitlm<-lm(Target~., data=TrainD)
-
-lmpred<-predict(fitlm, newdata = TestD)
-(MSElm<-mean((lmpred-TestD$Target)^2))
-
-# Compute the MSE for the Lasso
-xTest<-model.matrix(Target ~ ., TestD)[, -1]
-yTest<-TestD$Target
-
-#Create Predictions for Actual Test
-xFinalPreds<-model.matrix( ~ ., TestSet)[, -1]
-yFinalPreds<-TestD$Target
-
-#======================Run the Lasso======================
+bestlam <- cv.out$lambda.min
+fitlm<-lm(Target~., data=Train)
+lmpred<-predict(fitlm, newdata = Test)
 lasso.mod <- glmnet(x, y, alpha=1, lambda=bestlam)
-lasso.pred <- predict(lasso.mod, newx=xTest,s=bestlam, exact = T)
+lasso.pred <- predict(lasso.mod , newx=xTest,s=bestlam, exact = T)
 
-ValB$LassoTest <- lasso.pred
-ValB <- mutate(.data=ValB,LassoTest=case_when(LassoTest>.5~">50%",TRUE~"<50%"))
-confusion3 <- table(ValB$Target,ValB$LassoTest,dnn=c("Actual","Predicted"))
-fit_3_Pred <- print(paste("Fraction of Fit 3 Correct Predictions:", round((confusion3["<50%","<50%"]+confusion3[">50%",">50%"])/sum(confusion3),4)))
-
-lasso.pred1 <- predict(lasso.mod, newx=xFinalPreds,s=bestlam, exact = T)
-
-(MSElasso<-mean((lasso.pred-TestD$Target)^2))
-
-TestD$LassoPredValues <- lasso.pred #Attach back to the 
-TestD$TreePredValues <- tree.pred #Attach back to the 
-TestD <- mutate(.data=TestD,LassoPredValues=case_when(LassoPredValues>.5~1, TRUE~0))
-TestD <- mutate(.data=TestD,TreePredValues=case_when(TreePredValues>.5~1, TRUE~0))
-
-fourfoldplot(confusion1, color = c("#CC6666", "#99CC99"), conf.level = 0, margin = 1, main = "Confusion Matrix Top Attributes")
-fourfoldplot(confusion2, color = c("#CC6666", "#99CC99"), conf.level = 0, margin = 1, main = "Confusion Matrix Top Attributes")
-fourfoldplot(confusion3, color = c("#CC6666", "#99CC99"), conf.level = 0, margin = 1, main = "Confusion Matrix Top Attributes")
+Test$LassoTest <- lasso.pred
 
 
-###########
-### MLR ###
-###########
+#Change Test Values. 
+Test$Target <- factor(Test$Target,levels=c(0,1),labels = c("<50%",">50%"),ordered = FALSE)
+
+#Change from Values to <50%
+Test <- mutate(.data=Test,Binnomial =case_when(Binnomial>.5~">50%",TRUE~"<50%"))
+Test <- mutate(.data=Test,TreeTest  =case_when(TreeTest>.5~">50%",TRUE~"<50%"))
+Test <- mutate(.data=Test,LassoTest =case_when(LassoTest>.5~">50%",TRUE~"<50%"))
+
+confusion1 <- table(Test$Target,Test$Binnomial, dnn=c("Actual","Predicted"))
+confusion2 <- table(Test$Target,Test$TreeTest,  dnn=c("Actual","Predicted"))
+confusion3 <- table(Test$Target,Test$LassoTest, dnn=c("Actual","Predicted"))
+
+fit_1_Pred <- print(paste("Fraction of BI Correct Predictions:", round((confusion1["<50%","<50%"]+confusion1[">50%",">50%"])/sum(confusion1),4)))
+fit_2_Pred <- print(paste("Fraction of Tree Correct Predictions:", round((confusion2["<50%","<50%"]+confusion2[">50%",">50%"])/sum(confusion2),4)))
+fit_3_Pred <- print(paste("Fraction of Lasso Correct Predictions:", round((confusion3["<50%","<50%"]+confusion3[">50%",">50%"])/sum(confusion3),4)))
+
+fourfoldplot(confusion1, color = c("#CC6666", "#99CC99"), conf.level = 0, margin = 1, main = "Confusion Matrix Top Attributes [Binomal]")
+fourfoldplot(confusion2, color = c("#CC6666", "#99CC99"), conf.level = 0, margin = 1, main = "Confusion Matrix Top Attributes [Tree]")
+fourfoldplot(confusion3, color = c("#CC6666", "#99CC99"), conf.level = 0, margin = 1, main = "Confusion Matrix Top Attributes [Lasso]")
+
+
+########Create Predections with Testing stuff########
+biPred <- predict(fit1, type="response",newdata = TestSet)
+lasso.predTestSet <- predict(lasso.mod, newx=xFinalPreds,s=bestlam, exact = T)
+tree.predTestSet <- predict(model.tree, newdata = TestSet)
+
+TestSet$BinomialPredications <- biPred
+TestSet$lassoPredictions <- tree.predTestSet 
+TestSet$TreePredictions <- lasso.predTestSet
+
+
+TestSet <- mutate(.data=TestSet,BinomialPredications =case_when(BinomialPredications >.5~">50%",TRUE~"<50%"))
+TestSet <- mutate(.data=TestSet,lassoPredictions     =case_when(lassoPredictions     >.5~">50%",TRUE~"<50%"))
+TestSet <- mutate(.data=TestSet,TreePredictions      =case_when(TreePredictions      >.5~">50%",TRUE~"<50%"))
+
+#####################################################
 
 classifTask <- makeClassifTask(data = Train,target = "Target")
 kfold <- makeResampleDesc("RepCV",folds = 5,reps = 20)
+
 #### Binomial, K=10 ####
-
-kfoldCVBIN <- resample(learner=makeLearner("classif.binomial"),
-                       classifTask,resampling = kfold,
-                       show.info = FALSE)
-
+kfoldCVBIN <- resample(learner=makeLearner("classif.binomial"), classifTask,resampling = kfold, show.info = FALSE)
 CMBIN <- calculateConfusionMatrix(kfoldCVBIN$pred)
 AccuracyBIN <- (CMBIN$result[1]+CMBIN$result[5])/(CMBIN$result[1]+CMBIN$result[2]+CMBIN$result[4]+CMBIN$result[5])
 
-#### LASSO, K=10 ####
-kfoldCVLAS <- resample(learner=makeLearner("classif.glmnet"), 
-                       classifTask, 
-                       resampling = kfold,show.info = FALSE)
+#### TREE, K=10 ####
+kfoldCVTree <- resample(learner=makeLearner("classif.rpart"), classifTask, resampling = kfold,show.info = FALSE)
+CMTree        <- calculateConfusionMatrix(kfoldCVTree$pred)
+AccuracyTree<- (CMTree$result[1]+CMTree$result[5])/(CMTree$result[1]+CMTree$result[2]+CMTree$result[4]+CMTree$result[5])
 
-CMLAS <- calculateConfusionMatrix(kfoldCVLAS$pred)
+#### LASSO, K=10 ####
+kfoldCVLAS   <- resample(learner=makeLearner("classif.glmnet"), classifTask, resampling = kfold,show.info = FALSE)
+CMLAS        <- calculateConfusionMatrix(kfoldCVLAS$pred)
 AccuracyLAS <- (CMLAS$result[1]+CMLAS$result[5])/(CMLAS$result[1]+CMLAS$result[2]+CMLAS$result[4]+CMLAS$result[5])
 
-#### Random Forest, K=10 ####
-kfoldCVRF<-resample(learner=makeLearner("classif.rpart"), classifTask, resampling = kfold,show.info = FALSE)
-CMRF <- calculateConfusionMatrix(kfoldCVRF$pred)
-AccuracyRF <- (CMRF$result[1]+CMRF$result[5])/(CMRF$result[1]+CMRF$result[2]+ CMRF$result[4]+CMRF$result[5])
+print("")
+print("")
 
-#### KNN, K=10 ####
-kfoldCVKKNN<-resample(learner=makeLearner("classif.kknn"), classifTask, resampling = kfold,show.info = FALSE)
-CMKKNN<-calculateConfusionMatrix(kfoldCVKKNN$pred)
-AccuracyKKNN <- (CMKKNN$result[1]+CMKKNN$result[5])/(CMKKNN$result[1]+CMKKNN$result[2] +CMKKNN$result[4]+CMKKNN$result[5])
-print('Finished :)')
+dsfdsf <-  print(paste("Binmomial Accuracy:",round(AccuracyBIN,2)))
+print(paste( "Tree Accuracy:", round(AccuracyTree,2)))
+print(paste("Lasso Accuracy:", round(AccuracyLAS,2)))
